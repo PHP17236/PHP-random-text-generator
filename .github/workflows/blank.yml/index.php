437d450656e9b20932a12e36fb98be39577ac65a
Çağan Bora Eldenk <67594230+PHP17236@users.noreply.github.com>
2021-06-22 12:33:27 +0300

    index.php
    here is the code

---
+++ .github/workflows/blank.yml/index.php
@@ -0,0 +1,4 @@
+<?php 
+random_text  = array("num1", "num2", "num3"); # We create a array
+echo random_text(rand(0, 2)); # If you add more elements in the array change the last number in the rand to last indis number
+?>
